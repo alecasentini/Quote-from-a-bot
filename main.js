0854de77485f0e2a9c514debd64acf2d18e3f3db
alecasentini <ale.casentini@gmail.com>
2023-08-09 22:04:44 +0200

    Aggiunta action casuali + fix style

--- main.js
+++ main.js
@@ -2,9 +2,9 @@
 
 // Variabili
 
-const API_URL = "https://api.openai.com/v1/completions";
+const API_URL = "https://api.openai.com/v1/chat/completions";
 const MODEL = "gpt-3.5-turbo";
-const API_KEY = "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";
+const API_KEY = "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";
 
 const loader = document.querySelector(".loading-hidden");
 
@@ -19,13 +19,14 @@ const modalClose = document.querySelector('.modal-close');
 async function playCharacter (nameCharacter){
     loader.classList.remove("loading-hidden");
 
-    const action = "Saluta nel tuo modo più iconico";
+    // const action = "Saluta nel tuo modo più iconico";
+    const action = getRandomAction();
     const temperature = 0.7;
 
-    const response = await fetch(API_URL,{
+    const response = await fetch(API_URL, {
         method: "POST",
         headers: {
-            "Content-type": "application/json",
+            "Content-Type": "application/json",
             "Authorization": `Bearer ${API_KEY}`
         },
         body: JSON.stringify({
@@ -42,17 +43,35 @@ async function playCharacter (nameCharacter){
     })
 
     const data = await response.json();
+    console.log(data)
 
-    const message = data.choices[0].message.content;
+    const content = data.choices[0].message.content;
 
-    modalContent.innerHTML = `<h2>${nameCharacter}</h2>
-    <p>${message}</p>
-    <code>Character: ${nameCharacter}, action: ${action}, temperature: ${temperature}</code>`;
+    modalContent.innerHTML = `
+    <h2>${nameCharacter}</h2>
+    <p>${content}</p>
+    <code>Character: ${nameCharacter}, action: ${action}, temperature: ${temperature}</code>
+    `;
 
     loader.classList.add("loading-hidden");
     modal.classList.remove("modal-hidden");
 }
 
+
+function getRandomAction(){
+    const actions = [
+        'salutare nel tuo modo più iconico',
+        'dare un consiglio di stile in base ai tuoi gusti',
+        'raccontare la tua ultima avventura',
+        'svelarmi i tuoi sogni',
+        'dirmi chi è il tuo migliore amico',
+        'scrivere la tua bio di linkedin'
+    ];
+
+    const indexRandom = Math.floor(Math.random() * actions.length);
+
+    return actions[indexRandom];
+}
 
 // Init
 
